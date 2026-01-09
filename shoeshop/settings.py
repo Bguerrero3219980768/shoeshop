91eeb95733ed2189ff163d30ed6b8adcd1c9b024
brayanguerreo123 <112359242+Bguerrero3219980768@users.noreply.github.com>
2026-01-09 15:24:23 -0500

    render4

--- shoeshop/settings.py
+++ shoeshop/settings.py
@@ -151,6 +151,15 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
